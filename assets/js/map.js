230bef9ee230a91af04c1026fb49c41f36beb041
Lo Selby <laurenxselby@gmail.com>
2021-12-19 20:35:38 -0800

    local storage search history & search history in nav creation

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,11 +3,14 @@ var map = document.querySelector("#map");
 var input = document.querySelector("#search");
 let coordinates = JSON.parse(localStorage.getItem("brewcord")) || [];
 let history = JSON.parse(localStorage.getItem("search-history")) || [];
-// var breweryLocation = 'Vashon,WA';
 var tableContainer = document.querySelector("#brewery-table");
-// var mapBtn = document.querySelector(".map-btn");
+var histContainer = document.querySelector("#dropdown1");
+var clearBtn = document.querySelector("#clear-btn");
 
+// activates the history dropdown in the navbar
+$('.dropdown-trigger').dropdown();
 
+// event listener when the enter key is pressed while the user has their cursor in the search bar
 input.addEventListener("keydown", function(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
@@ -17,18 +20,19 @@ input.addEventListener("keydown", function(event) {
     }
   });
 
-
+// function that triggers the other functions when the form is submitted (and sets the coordinate data to the local storage)
 function formSubmit(event) {
     event.preventDefault();
     var cityName = input.value;
     generateBreweryData(cityName);
     history.push(cityName);
     localStorage.setItem("search-history", JSON.stringify(history));
-    console.log(cityName);
+    renderSearchHistBtns ();
     input.value = "";
   }
 
 
+// api fetch function that fetches brewery data for a city, and creates the list elements
 function generateBreweryData (cityName) {
     tableContainer.innerHTML = "";
     console.log(cityName);
@@ -39,98 +43,110 @@ function generateBreweryData (cityName) {
 
     fetch(brewRequestURL)
     .then(function (response) {
-        console.log(response);
     if (response.ok) {
       response.json().then(function (data) {
         console.log(data);
 
 
-        // for loop to create table elements
-        for (let i =0; i < data.length; i++) {
+                // for loop to create table elements
+                for (let i =0; i < data.length; i++) {
 
-            var rowEl = document.createElement('tr');
-            tableContainer.append(rowEl);
+                    var rowEl = document.createElement('tr');
+                    tableContainer.append(rowEl);
 
-            var rowContent = document.createElement('td');
-            // rowContent.classList.add("col s8");
-            rowEl.append(rowContent);
+                    var rowContent = document.createElement('td');
+                    rowEl.append(rowContent);
 
-            var brewName = document.createElement('p');
-            brewName.classList.add("brew-name");
-            brewName.textContent = data[i].name + " [" + data[i].brewery_type + "]" ;
+                    var brewName = document.createElement('p');
+                    brewName.classList.add("brew-name");
+                    brewName.textContent = data[i].name + " [" + data[i].brewery_type + "]" ;
 
-            var breweryAddress = document.createElement('p');
-            var addressFull = "";
-            if (data[i].street) {
-              addressFull += data[i].street + " " + data[i].city + ", " + data[i].state + " " + data[i].postal_code;
-            } else {
-              "brew-name".remove();
-            }
+                    var breweryAddress = document.createElement('p');
+                    var addressFull = "";
 
-            breweryAddress.textContent = addressFull;
+                    // if else to remove any breweries that feature null address data
+                    if (data[i].street) {
+                    addressFull += data[i].street + " " + data[i].city + ", " + data[i].state + " " + data[i].postal_code;
+                    } else {
+                    rowEl.remove();
+                    }
 
-            var brewWebsite = document.createElement('a');
-            brewWebsite.classList.add("brew-website");
-            brewWebsite.href = data[i].website_url;
-            brewWebsite.textContent = data[i].website_url;
+                    breweryAddress.textContent = addressFull;
 
-            var buttonContainer = document.createElement('td');
-            buttonContainer.classList.add ("mapbtn-container");
-            // rowContent.classList.add("col s4");
+                    var brewWebsite = document.createElement('a');
+                    brewWebsite.classList.add("brew-website");
+                    brewWebsite.href = data[i].website_url;
+                    brewWebsite.textContent = data[i].website_url;
+
+                    var buttonContainer = document.createElement('td');
+                    buttonContainer.classList.add ("mapbtn-container");
+                    
+                    var buttonValue = data[i].latitude + "," + data[i].longitude;
+
+                    coordinates.push(buttonValue);
+                    localStorage.setItem("brewcord", JSON.stringify(coordinates));
+                    console.log(coordinates);
+
+                        // buttons to view the brewery on a map
+                        const mapButton = document.createElement('button');
+                        mapButton.className = " map-btn btn waves-effect grey darken-4";
+                        mapButton.setAttribute("type", "submit");
+                        mapButton.textContent = "Map it!";
+                        mapButton.setAttribute("value", coordinates[i]);
+
+                        // icon on the map buttons
+                        var btnIcon = document.createElement("i");
+                        btnIcon.className = "material-icons left";
+                        btnIcon.textContent = "location_searching";
+                        mapButton.append(btnIcon);
             
-            var buttonValue = data[i].latitude + "," + data[i].longitude;
-            // console.log(buttonValue);
+                        buttonContainer.append(mapButton);
 
-            coordinates.push(buttonValue);
-            localStorage.setItem("brewcord", JSON.stringify(coordinates));
-            console.log(coordinates);
-
-                // buttons to view the brewery on a map
-                const mapButton = document.createElement('button');
-                mapButton.className = " map-btn btn waves-effect grey darken-4";
-                mapButton.setAttribute("type", "submit");
-                mapButton.textContent = "Map it!";
-                mapButton.setAttribute("value", coordinates[i]);
-                // mapButton.setAttribute("data-cord", buttonValue);
-                // console.log(mapButton.dataset.cord);
-                // icon on the map buttons
-                var btnIcon = document.createElement("i");
-                btnIcon.className = "material-icons left";
-                btnIcon.textContent = "location_searching";
-                mapButton.append(btnIcon);
-      
-                buttonContainer.append(mapButton);
-                // const mapBtn = document.querySelector(".map-btn");
-                mapButton.addEventListener("click", event => {
-                    console.log(event.target.value);
-                    generateMap(event.target.value);
-                })
+                        // event listener for the map buttons to start the gerneateMap function on click
+                        mapButton.addEventListener("click", event => {
+                            console.log(event.target.value);
+                            generateMap(event.target.value);
+                        })
           
-            
-            rowEl.append(rowContent, buttonContainer);
-            rowContent.append(brewName, breweryAddress, brewWebsite);
-
-            // mapButton.addEventListener("click",function() {
-            //     map.src="";
-            //     var mapRequestURL = 'https://maps.googleapis.com/maps/api/staticmap?zoom=16&size=350x300&scale=1&maptype=roadmap&markers=size%3Alrg%7Ccolor%3Ablack%7C' + mapButton.value + '&center=' + mapButton.value + 'key=AIzaSyBPtCzgyimy69Svl3-LRgwO47gFXn8XAyI';
-            //     console.log(mapRequestURL);
-            //     map.src = mapRequestURL;
-            //     // generateMap(mapButton.value);
-            // })
+                // appends elements to each other on the brewery list
+                rowEl.append(rowContent, buttonContainer);
+                rowContent.append(brewName, breweryAddress, brewWebsite);
 
         }
-
         })
+         }
+        })
+        }
+
+
+    // function to create the search history buttons
+    function renderSearchHistBtns() {
+        for (let i=0; i<history.length; i++) {
+            const historyLI = document.createElement("li");
+            histContainer.append(historyLI);
+            const historyBtn = document.createElement("button");
+            historyBtn.setAttribute("type","text");
+            historyBtn.setAttribute("readonly",true);
+            historyBtn.setAttribute("class", "btn waves-effect grey darken-4");
+            historyBtn.textContent = history[i];
+            historyBtn.setAttribute("value", history[i]);
+            historyBtn.addEventListener("click",function() {
+                localStorage.removeItem("brewcord");
+                 coordinates = [];
+                generateBreweryData(historyBtn.value);
+            })
+            historyLI.append(historyBtn);
+        }
+        }
+        
+// runs this function on page load so the history buttons are always there, fixes bug that was generating an extra button
+    renderSearchHistBtns();
+    if (history.length > 0) {
+        generateBreweryData(history[history.length - 1]);
     }
-    })
-}
 
 
-
-// generateBreweryData();
-
-
-
+// function to generate the map data on button click
 function generateMap (breweryLocation) {
     console.log(breweryLocation);
     var mapRequestURL = 'https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=250x250&scale=2&maptype=roadmap&markers=size%3Alrg%7Ccolor%3Ablack%7C' + breweryLocation + '&center=' + breweryLocation + '&key=AIzaSyBPtCzgyimy69Svl3-LRgwO47gFXn8XAyI';
@@ -139,8 +155,9 @@ function generateMap (breweryLocation) {
     
 }
 
-// mapBtn.addEventListener("submit", event => {
-//     console.log(event.target.value);
-//     generateMap(event.target.value);
-// })
-
+// if you click the clear button, it will clear the local storage for search history & reload the page to clear the buttons, and bring you back to the main page
+clearBtn.addEventListener("click",function() {
+    localStorage.removeItem("search-history");
+    history = [];
+    location.reload();
+  })
